10df6e113600e5cf88789b80b3526b4fb33d8493
JCWoodworker <jfc3303@gmail.com>
2022-02-13 12:04:48 -0500

    homepage updates

--- client/src/components/layout/HomePage.js
+++ client/src/components/layout/HomePage.js
@@ -3,15 +3,9 @@ import React from "react"
 const HomePage = props => {
   return (
     <div className="page-container">
-      <h1>Welcome to the Project Pricing App!</h1>
-      <h3>Upcoming Features:</h3>
-      <p></p>
-      <p className="complete">Sign in and out</p> 
-      <p className="complete">Display your local weather</p> 
-      <p className="complete">Display sponsor with link to their website</p>
-      <p className="complete">Signed-in homepage</p>
-      <p>Intro Video</p>
-      <p>Sign up is free!</p>
+      <h1>Welcome to the App!</h1>
+      <p>*** Currently under construction ***</p>
+      <p>The app will be FREE to use!</p>
     </div>
   )
 }
