63e8a8b61300254795aec89e24859d4c27c350d9
James Corey <jc@Jamess-MacBook-Pro.local>
2022-02-22 09:22:00 -0500

    updated dev info page

--- client/src/components/layout/DevInfoPage.js
+++ client/src/components/layout/DevInfoPage.js
@@ -8,7 +8,13 @@ const DevInfoPage = props => {
         <img className="dev-image" src="https://i.imgur.com/3t3Ou80.jpg?1" />
         <div className="dev-details">
           <h1 className="page-heading">James Corey</h1>
-          <p>Developer info coming soon!</p>
+          <p>Software Engineer / Woodworker / Dog Dad</p>
+          <ul>Quick Facts:</ul>
+            <li>Just finished an immersive, 20 week full stack coding bootcamp at Launch Academy in Boston, MA</li>
+            <li>Long work history in sales (mainly automotive) and restaurant management</li>
+            <li>Always the go-to tech guy in every job no matter what the industry</li>
+            <li>Owner/Maker at RI Local Woodworks, LLC</li>
+            <li>Excited to put the creative and problem-solving halves of my brain together with a career in software development!</li>
         </div>
       </div>
     </div>
