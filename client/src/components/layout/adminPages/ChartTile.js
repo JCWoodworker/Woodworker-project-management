44528ccd4ae0f448bfccf6097f65c9a90df29ee4
James Corey <jfc3303@gmail.com>
2022-02-28 12:17:36 -0500

    commented out code during refactor

--- client/src/components/layout/adminPages/ChartTile.js
+++ client/src/components/layout/adminPages/ChartTile.js
@@ -1,34 +1,55 @@
 import React from "react"
+// import { Chart } from 'chart.js/auto'
+// import { Pie } from 'react-chartjs-2'
 
 const ChartTile = props => {
 
-  google.charts.load('current', {'packages':['corechart']})
-  google.charts.setOnLoadCallback(drawChart)
+// const data = {
+//   labels: ['label1', 'label2'],
+//   datasets: [
+//     {
+//       label: 'These are things',
+//       backgroundColor: ['#46bfbd', '#f746a4'],
+//       borderWidth: 1,
+//       data: props.woodDta
+//     }
+//   ]
+// }
 
-  function drawChart() {
-    
-    var data = new google.visualization.DataTable()
-    data.addColumn('string', 'Hardwood')
-    data.addColumn('number', 'BoardFeet')
-    data.addRows(props.woodData)
-
-
-    var options = {
-      'title': `Top Woods by the boardfoot in customers' projects`,
-      'legend': 'bottom',
-      'width': 500,
-      'height': 600,
-    }
-
-    var chart = new google.visualization.BarChart(document.getElementById('hardwoodChart'))
-    chart.draw(data, options)
-
-  }
-
+// const options = {
+//   title: {text: "title text", display: true},
+//   legend: {display: true}
+// }
 
   return (
-    <div id="hardwoodChart"></div>
+    <div id="hardwoodChart">
+      {/* <Pie data={data} /> */}
+    </div>
   )
 }
 
 export default ChartTile
+
+
+// google.charts.load('current', {'packages':['corechart']})
+// google.charts.setOnLoadCallback(drawChart)
+
+// function drawChart() {
+  
+//   var data = new google.visualization.DataTable()
+//   data.addColumn('string', 'Hardwood')
+//   data.addColumn('number', 'BoardFeet')
+//   data.addRows(props.woodData)
+
+
+//   var options = {
+//     'title': `Top Woods by the boardfoot in customers' projects`,
+//     'legend': 'bottom',
+//     'width': 500,
+//     'height': 600,
+//   }
+
+//   var chart = new google.visualization.BarChart(document.getElementById('hardwoodChart'))
+//   chart.draw(data, options)
+
+// }
