a4a6aa89523d921158e4147c5ddebbbd492210a0
James Corey <jfc3303@gmail.com>
2022-03-24 23:22:18 -0400

    changed on hover effect for project pictures

--- client/src/services/projectCalculations.js
+++ client/src/services/projectCalculations.js
@@ -1,9 +1,9 @@
 export const calculations = (woodCost, projectHours, labor, markup) => {
-  console.log(`*********`)
-  console.log(`LABOR: ${labor}`)
-  console.log(`MARKUP: ${markup}%`)
-  console.log(`WOOD COST: $${woodCost}`)
-  console.log(`PROJECT HOURS: ${projectHours}`)
+  // console.log(`*********`)
+  // console.log(`LABOR: ${labor}`)
+  // console.log(`MARKUP: ${markup}%`)
+  // console.log(`WOOD COST: $${woodCost}`)
+  // console.log(`PROJECT HOURS: ${projectHours}`)
 
   woodCost = parseInt(woodCost)
   projectHours = parseInt(projectHours)
@@ -13,11 +13,11 @@ export const calculations = (woodCost, projectHours, labor, markup) => {
   const projectCost = parseInt(laborCost + woodCost).toFixed(2)
   const retailPrice = parseInt(projectCost) * markup
   
-  console.log(`*********`)
-  console.log(`LABOR: $${laborCost}`)
-  console.log(`TOTAL PROJECT COST: $${projectCost}`)
-  console.log(`RETAIL PRICE: $${retailPrice}`)
-  console.log(`*********`)
+  // console.log(`*********`)
+  // console.log(`LABOR: $${laborCost}`)
+  // console.log(`TOTAL PROJECT COST: $${projectCost}`)
+  // console.log(`RETAIL PRICE: $${retailPrice}`)
+  // console.log(`*********`)
 
   return retailPrice
 
