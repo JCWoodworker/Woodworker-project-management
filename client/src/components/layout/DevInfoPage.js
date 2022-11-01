e9bc50f1dfe373ae353f4fba84a6d3e9ff9825f5
James Corey <jc@rilocalwoodworks.com>
2022-11-01 00:57:27 -0400

    updated styling, added weather, updated dev info

--- client/src/components/layout/DevInfoPage.js
+++ client/src/components/layout/DevInfoPage.js
@@ -6,16 +6,17 @@ const DevInfoPage = props => {
   return (
     <div className="home-page-container">
       <div className="dev-info-container">
-        <img className="dev-image" src="https://i.imgur.com/3t3Ou80.jpg?1" />
+        <img className="dev-image" src="https://i.imgur.com/oLEqhuJl.jpg" />
         <div className="dev-details">
           <h1 className="page-heading">James Corey</h1>
-          <p>Software Engineer / Woodworker / Dog Dad</p>
+          <p>Software Engineer / Woodworker / Dog Dad / Father / Skater / Ninja Warrior</p>
           <ul>Quick Facts:</ul>
-            <li>Just finished an immersive, 20 week full stack coding bootcamp at Launch Academy in Boston, MA</li>
-            <li>Long work history in sales (mainly automotive) and restaurant management</li>
+            <li>CURRENT ROLE: Ops Engineer @ Grubhub (Boston, MA)</li>
+            <li>Completed immersive, 20 week, full stack coding bootcamp at Launch Academy in Boston, MA (Feb, 2022)</li>
+            <li>Long work history in sales (mainly automotive), restaurant management, and hospitality</li>
             <li>Always the go-to tech guy in every job no matter what the industry</li>
-            <li>Owner/Maker at RI Local Woodworks, LLC</li>
-            <li>Excited to put the creative and problem-solving halves of my brain together with a career in software development!</li>
+            <li>Owner/Maker/Woodworker at RI Local Woodworks, LLC</li>
+            <li>Excited to be putting the creative and problem-solving halves of my brain together with a career in software development!</li>
             <Link to='/'>
               <button 
                 id="all-buttons">
