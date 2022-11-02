b056f2ac5102f90e7f02af090330106818d79627
James Corey <jc@rilocalwoodworks.com>
2022-11-01 23:43:11 -0400

    user can now change a customer status via drop down menu that appears after clicking the current status

--- server/src/config.js
+++ server/src/config.js
@@ -2,13 +2,6 @@ import "./boot.js";
 import getNodeEnv from "./config/getNodeEnv.js";
 import getDatabaseUrl from "./config/getDatabaseUrl.cjs";
 
-// export default {
-//   nodeEnv: getNodeEnv(),
-//   session: { secret: process.env.SESSION_SECRET },
-//   weatherKey: process.env.WEATHER_API,
-//   databaseUrl: getDatabaseUrl(getNodeEnv()),
-//   web: { host: process.env.HOST || "0.0.0.0", port: process.env.PORT || 3000 }
-// };
 const development = {
   weatherKey: process.env.WEATHER_API,
   awsAccess: { key: process.env.AWS_ACCESS_KEY },
