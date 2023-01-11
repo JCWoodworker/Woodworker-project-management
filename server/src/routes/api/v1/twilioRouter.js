3dbf0cf021a4ba192077eb7c181f7389f6c3df4b
James Corey <jc@rilocalwoodworks.com>
2023-01-10 23:32:49 -0500

    integrated twilio for sending sms messages to customers in a user's crm.  all seeded cell phones are my business phone for RI Local Woodworks, which is publicly available just by doing an online search anyway

---
+++ server/src/routes/api/v1/twilioRouter.js
@@ -0,0 +1,42 @@
+import express from "express"
+import twilio from "twilio"
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+
+const twilioRouter = new express.Router()
+
+twilioRouter.get("/", (req, res) => {
+  res.send("Twilio Router for Woodworker's Project Management App.  No GET requests here.")
+})
+
+
+twilioRouter.post("/outgoingMessages", async (req, res) => {
+  try {
+    const client = new twilio(accountSid, authToken)
+    const { body } = req
+    const message = await client.messages
+      .create({
+        body: `${body.message}`,
+        from: '+14012102221',
+        to: `+1${body.sendPhone}`
+      })
+    console.log(message.sid)
+    return res.status(200).json({ message: "Successful POST Request" })
+  } catch (error) {
+    return res.status(500).json({ error: error })
+  }
+})
+
+twilioRouter.post("/incomingMessages", async (req, res) => {
+  try {
+    const incomingMessageData = req.body
+    console.log(incomingMessageData)
+  } catch (error) {
+    return res.status(500).json({ error: error })
+  }
+})
+
+
+
+
+export default twilioRouter
