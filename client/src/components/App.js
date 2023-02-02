4935f153482087bfb191d140eaa6cfe032fc5617
James Corey <jc@rilocalwoodworks.com>
2023-02-01 19:14:12 -0500

    moved main.scss import

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { hot } from "react-hot-loader/root"
+import "../assets/scss/main.scss"
 
 import getCurrentUser from "../services/getCurrentUser"
-import "../assets/scss/main.scss"
 import RegistrationForm from "./registration/RegistrationForm"
 import SignInForm from "./authentication/SignInForm"
 import TopBar from "./layout/TopBar"
